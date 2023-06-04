ca643fe0e646daaffe7a9439735384bc98663c8b
dominikmendoza <u20201b980@upc.edu.pe>
2023-06-04 15:59:55 -0500

    Jupyter notebook presentation

--- app/app.py
+++ app/app.py
@@ -1,4 +1,6 @@
 from flask import Flask, render_template
+import nbformat
+from nbconvert import HTMLExporter
 
 app = Flask(__name__)
 
@@ -6,5 +8,19 @@ app = Flask(__name__)
 def index():
     return render_template('index.html')
 
+@app.route('/notebook')
+def notebook():
+    # Lee el archivo del notebook
+    with open('./app/src/Program_Huaman_Mendoza_Ramirez.ipynb') as f:
+        notebook_content = f.read()
+        notebook = nbformat.reads(notebook_content, as_version=4)
+    
+    # Convierte el notebook a HTML
+    html_exporter = HTMLExporter()
+    #html_exporter.template_file = 'basic'  # Puedes usar otro template si lo deseas
+    #html_exporter.template_path.append('./app/templates')
+    (body, _) = html_exporter.from_notebook_node(notebook)
+
+    return render_template('notebook.html', body=body)
 if __name__ == '__main__':
     app.run(debug=True)
